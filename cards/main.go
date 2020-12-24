7182b67fcf5e3b673ce5008b8100b29b470eb801
Thiago Jamir <tjamir@gmail.com>
2020-12-24 13:01:32 -0300

    Adding shuffle feature

--- cards/main.go
+++ cards/main.go
@@ -3,12 +3,16 @@ package main
 func main() {
 	cards := newDeck()
 
-	hand, _ := deal(cards, 3)
+	cards.shuffle()
+
+	hand, remaining := deal(cards, 3)
 
 	hand.saveToFile("my_cards")
 
 	deck := readFromFile("my_cards")
 	deck.print()
+
+	remaining.print()
 
 }
 
