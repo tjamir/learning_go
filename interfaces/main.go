1e34438e064599c35a582b42722641497074cf38
Thiago Jamir <tjamir@gmail.com>
2020-12-29 10:57:19 -0300

    Adding intefaces

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+
+type spanishBot struct{}
+
+type bot interface {
+	getGreeting() string
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+
+}
+
+func (englishBot) getGreeting() string {
+	return "Hey there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
