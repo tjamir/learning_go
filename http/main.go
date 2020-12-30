3aa814d7612a37b75ff7ce9052feb36626c4566c
Thiago Jamir <tjamir@gmail.com>
2020-12-30 10:06:25 -0300

    Final version of the http project

--- http/main.go
+++ http/main.go
@@ -2,15 +2,55 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	resp, error := http.Get("http://google.com")
+	readHTMLHard()
+
+}
+
+func readHTMLEasy() {
+	resp, error := http.Get("http://www.google.com")
 	if error != nil {
 		fmt.Println("Error", error)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	io.Copy(os.Stdout, resp.Body)
+}
+
+func readHTMLHard() {
+	resp, error := http.Get("http://www.google.com")
+	if error != nil {
+		fmt.Println("Error", error)
+		os.Exit(1)
+	}
+	bytes := make([]byte, 0)
+	//fmt.Println(resp)
+	read := 1
+
+	eof := false
+
+	for read > 0 && !eof {
+		temp := make([]byte, 128)
+		n, error := resp.Body.Read(temp)
+		if n < 128 {
+			temp = temp[:n]
+		}
+		bytes = append(bytes, temp...)
+		if error != nil {
+			if error.Error() == "EOF" {
+				eof = true
+			} else {
+				fmt.Println("Error", error)
+				os.Exit(2)
+			}
+		}
+		read = n
+
+	}
+	resp.Body.Close()
+	fmt.Println(string(bytes))
 }
